07e034f04eea2c4ac628561416b5db8009a00df6
MersenneInteger <zachjohnsonsoftware@gmail.com>
2019-09-07 13:24:38 -0500

    Update phonebook.py

--- Phonebook/phonebook.py
+++ Phonebook/phonebook.py
@@ -3,20 +3,47 @@ from extronlib import event, Version
 from extronlib.device import eBUSDevice, ProcessorDevice, UIDevice
 from extronlib.interface import *
 from extronlib.ui import Button, Knob, Label, Level, Slider
-from extronlib.system import Clock, MESet, Timer, Wait
+from extronlib.system import Clock, MESet, Timer, Wait, File, ProgramLog
 
+import csv
 
 class Phonebook:
     
-   def __init__(self):
+    def __init__(self, file_name):
     
-        self._contacts = {} 
+        self._first_names = []
+        self._last_names = []
+        self._phone_nums = []
+        self._contacts = {}
+        self._file_name = '/user/' + file_name
+        self._csv_reader = None
+        self.read_file(self._file_name)
         
+    
+    def build_contacts(self, contact_info):
+    
+        for i in range(len(contact_info)):
+            self._first_names.append(contact_info[i]['first_name'])
+            self._last_names.append(contact_info[i]['last_name'])
+            self._phone_nums.append(contact_info[i]['phone_number'])
+            
+        for i in range(len(contact_info)):
+            contact = 'contact' + str(i+1)
+            self._contacts[contact] = [self._first_names[i], self._last_names[i], self._phone_nums[i]]
+    
     
     def read_file(self, file_name):
-        pass
+
+        try:
+            with File(file_name, 'r') as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                rows = list(csv_reader)
+            if rows != []:
+                build_contacts(rows)
+        except Exception as e:
+            ProgramLog('File not found: {0}\n{1}'.format(file_name, e))
         
-        
+
     def write_file(self, file_name):
         pass
         
