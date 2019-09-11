e7aa1d62957cd63229ede079081fd67ce408980c
MersenneInteger <zachjohnsonsoftware@gmail.com>
2019-09-11 13:48:20 -0500

    Create main.py

---
+++ Phonebook/main.py
@@ -0,0 +1,35 @@
+## Begin ControlScript Import --------------------------------------------------
+from extronlib import event, Version
+from extronlib.device import eBUSDevice, ProcessorDevice, UIDevice
+from extronlib.interface import *
+from extronlib.ui import Button, Knob, Label, Level, Slider
+from extronlib.system import Clock, MESet, Timer, Wait
+
+from phonebook import *
+
+## End ControlScript Import ----------------------------------------------------
+##
+## Begin User Import -----------------------------------------------------------
+
+## End User Import -------------------------------------------------------------
+##
+## Begin Device/Processor Definition -------------------------------------------
+
+## End Device/Processor Definition ---------------------------------------------
+##
+## Begin Device/User Interface Definition --------------------------------------
+
+## End Device/User Interface Definition ----------------------------------------
+##
+## Begin Communication Interface Definition ------------------------------------
+
+## End Communication Interface Definition --------------------------------------
+
+def Initialize():
+    pass
+
+## Event Definitions -----------------------------------------------------------
+
+## End Events Definitions-------------------------------------------------------
+
+Initialize()
