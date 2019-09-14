4d0a88b98260c5713552fcdb9c815edfd948dcd9
MersenneInteger <zachjohnsonsoftware@gmail.com>
2019-09-13 19:17:30 -0500

    fix label text

--- Phonebook/keyboard.py
+++ Phonebook/keyboard.py
@@ -1,10 +1,5 @@
 ## Begin ControlScript Import --------------------------------------------------
-from extronlib import event, Version
-from extronlib.device import eBUSDevice, ProcessorDevice, UIDevice
-from extronlib.interface import *
-from extronlib.ui import Button, Knob, Label, Level, Slider
-from extronlib.system import Clock, MESet, Timer, Wait
-
+from extronlib.system import Wait, ProgramLog
 '''
     The keyboard class requires a list of button objects to be passed in this
     exact order:
@@ -20,49 +15,58 @@ class Keyboard:
         self._key_map = {}
         keys = 'abcdefghijklmnopqrstuvwxyz0123456789-.*@#'
         self._key_map = dict(zip(button_list, keys))
+        print(self._key_map)
         self._is_caps_locked = False
         self._is_shift_on = False
         self._shift_btn = shift_btn
         self._caps_lock_btn = caps_lock_btn
 
 
-    def key_entered(button):
+    def key_entered(self, btn):
 
-        if self._is_caps_locked or self._is_shift_on:
-            self._keyboard_text = upper(self._key_map[button])
-            self._is_shift_on = False
-            if self._shift_btn != None:
-                self._shift_btn.SetState(0)
-        else:
-            self._keyboard_text = self._key_map[button]
-        self._keyboard_label = self._keyboard_text
+        try:
+            if self._is_caps_locked or self._is_shift_on:
+                self._keyboard_text += self._key_map[btn].upper()
+                self._is_shift_on = False
+                if self._shift_btn != None:
+                    self._shift_btn.SetState(0)
+            else:
+                self._keyboard_text += self._key_map[btn]
+            self._keyboard_label.SetText(self._keyboard_text)
+        except Exception as e:
+            ProgramLog('Error occured entering key in Keyboard: {0}'.format(e))
         
         
-    def clear():
+    def clear(self):
     
         self._keyboard_text = ''
-        self._keyboard_label = self._keyboard_text
+        self._keyboard_label.SetText(self._keyboard_text)
         
         
-    def backspace():
+    def backspace(self):
         
         if len(self._keyboard_text) > 0:
             self._keyboard_text = self._keyboard_text[:len(self._keyboard_text)-1]
-            self._keyboard_label = self._keyboard_text
+            self._keyboard_label.SetText(self._keyboard_text)
         
         
-    def shift():
+    def shift(self):
         
         self._is_shift_on = True
         if self._shift_btn != None:
             self._shift_btn.SetState(1)
         
         
-    def caps_lock():
+    def caps_lock(self):
         
         self._is_caps_locked = not self._is_caps_locked
         if self._caps_lock_btn != None:
             self._caps_lock_btn.SetState(self._is_caps_locked)
         
+        
+    def space(self):
+    
+        self._keyboard_text += ' '
+        self._keyboard_label.SetText(self._keyboard_text)
     
     
